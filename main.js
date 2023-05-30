45c8e24efa959b81b829253f3eae34150f6c57cf
mc0749050 <131736467+mc0749050@users.noreply.github.com>
2023-05-30 19:19:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("welcome to JavaScript");
+
+function opennev(){
+  document.getElementById("login").style.display = "inline"
+};
+
+function closenev(){
+  document.getElementById("login").style.display = "none"
+};
+
+
